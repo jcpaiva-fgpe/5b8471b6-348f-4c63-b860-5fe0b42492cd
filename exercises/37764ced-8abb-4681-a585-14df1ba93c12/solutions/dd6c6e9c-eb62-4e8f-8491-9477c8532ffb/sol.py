e8387f393e2fc1117932af62e9e9fcf5780fab0b
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-04 01:07:22 +0100

    updated file exercises/37764ced-8abb-4681-a585-14df1ba93c12/solutions/dd6c6e9c-eb62-4e8f-8491-9477c8532ffb/sol.py

--- exercises/37764ced-8abb-4681-a585-14df1ba93c12/solutions/dd6c6e9c-eb62-4e8f-8491-9477c8532ffb/sol.py
+++ exercises/37764ced-8abb-4681-a585-14df1ba93c12/solutions/dd6c6e9c-eb62-4e8f-8491-9477c8532ffb/sol.py
@@ -2,10 +2,12 @@ d = {}
 while True:
     print('Podaj wynik:')
     res = input().split()
-    if res[0] != 'koniec':
-        d[res[0]] = float(res[1])
-        maxv = max(d.values())
-        maxk = [k for k, v in d.items() if v == maxv] 
-        print('Lider:', *maxk, maxv)
-    else:
+    try:
+        if res[0] != 'koniec':
+            d[res[0]] = float(res[1])
+            maxv = max(d.values())
+            maxk = [k for k, v in d.items() if v == maxv] 
+            print('Lider:', *maxk, maxv)
+        else: break
+    except:
         break
