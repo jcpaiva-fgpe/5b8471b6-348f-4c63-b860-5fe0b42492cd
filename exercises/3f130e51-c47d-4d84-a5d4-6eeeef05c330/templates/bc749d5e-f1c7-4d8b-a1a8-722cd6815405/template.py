077a0db2a150a70dd08463c381f089c2d7fa0097
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-07 16:57:15 +0100

    updated file exercises/3f130e51-c47d-4d84-a5d4-6eeeef05c330/templates/bc749d5e-f1c7-4d8b-a1a8-722cd6815405/template.py

--- exercises/3f130e51-c47d-4d84-a5d4-6eeeef05c330/templates/bc749d5e-f1c7-4d8b-a1a8-722cd6815405/template.py
+++ exercises/3f130e51-c47d-4d84-a5d4-6eeeef05c330/templates/bc749d5e-f1c7-4d8b-a1a8-722cd6815405/template.py
@@ -1,3 +1,2 @@
 c = input().strip()
-#if __name__ == '__main__':
-{{if not len(c): print("Brak danych - sprawdź swoj kod. Upewnij się, że poprawnie używasz polecenia wczytującego tekst.")}}
+{{ code }}
