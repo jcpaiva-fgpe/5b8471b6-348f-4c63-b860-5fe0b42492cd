550f92a87a9872d6b15800b17a220d9bad14bfa8
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-19 03:06:40 +0100

    updated file exercises/255e4115-2f3d-43de-90aa-380e720e2274/solutions/9097ac4d-3073-4be6-bbaa-671486969948/sol.py

--- exercises/255e4115-2f3d-43de-90aa-380e720e2274/solutions/9097ac4d-3073-4be6-bbaa-671486969948/sol.py
+++ exercises/255e4115-2f3d-43de-90aa-380e720e2274/solutions/9097ac4d-3073-4be6-bbaa-671486969948/sol.py
@@ -1,3 +1,27 @@
-# Adamowicz 88072902015
-n, m = input().strip().split()
-print(n, m)
+while True:
+    print('Wpisz poniżej nazwisko i pesel obywatela:')
+    try:
+        name, pesel = map(str, input().split())
+        gender, last = int(pesel[9]), int(pesel[10])
+        sum_psl = int(pesel[0]) + int(pesel[1]) * 3 + int(pesel[2]) * 7 + int(pesel[3]) * 9 + int(pesel[4]) + int(pesel[5]) * 3 + int(pesel[6]) * 7 + int(pesel[7]) * 9 + int(pesel[8]) + gender * 3
+        d, m, r = pesel[4:6], pesel[2:4], pesel[:2]
+
+    except ValueError: break
+    
+    status = (10 - sum_psl % 10)
+    if status == last:
+       zwroty = 'Pani' if gender % 2 == 0 else 'Pan', 'a' if gender % 2 == 0 else 'y'
+       
+       r = f'19{r}' if int(m) < 20 else f'20{r}'
+       m = int(m) if int(m) < 20 else int(m) - 20
+       m = f'0{m}' if m < 10 else str(m) 
+    
+       data = f'{d}.{m}.{r}'
+       import datetime
+       aktualnie = datetime.datetime.now()
+       wiek = (aktualnie.year - int(r)) - ((aktualnie.year, aktualnie.month, aktualnie.day) > (int(r), int(m), int(d))) 
+
+    else: break
+
+    print(zwroty[0], f'{name.title()}, urodzon{zwroty[1]}: {data}, legitymując{zwroty[1]} się numerem PESEL: {pesel}, aktualnie ma: {wiek}', 'lat' if wiek<=21 and wiek>3 else 'lata')
+    
