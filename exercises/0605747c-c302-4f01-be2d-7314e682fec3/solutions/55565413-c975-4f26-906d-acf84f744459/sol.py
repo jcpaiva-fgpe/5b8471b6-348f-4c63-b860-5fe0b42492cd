7cbbbbaea764fc3681db95c48d49282c1e6d0940
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-19 23:51:17 +0100

    updated file exercises/0605747c-c302-4f01-be2d-7314e682fec3/solutions/55565413-c975-4f26-906d-acf84f744459/sol.py

--- exercises/0605747c-c302-4f01-be2d-7314e682fec3/solutions/55565413-c975-4f26-906d-acf84f744459/sol.py
+++ exercises/0605747c-c302-4f01-be2d-7314e682fec3/solutions/55565413-c975-4f26-906d-acf84f744459/sol.py
@@ -2,5 +2,5 @@ while True:
     try:
         print(sum(list(map(int, input()))))
     except ValueError:
-        print('Miałes wisać liczbę całkowitą!')
+        print('Miałes wpisać liczbę całkowitą!')
         break
