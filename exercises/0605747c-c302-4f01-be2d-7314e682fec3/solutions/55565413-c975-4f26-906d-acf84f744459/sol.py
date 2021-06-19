3c1994c65a4342f6125f3e1b1e88991c26c64dc9
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-19 23:48:03 +0100

    updated file exercises/0605747c-c302-4f01-be2d-7314e682fec3/solutions/55565413-c975-4f26-906d-acf84f744459/sol.py

--- exercises/0605747c-c302-4f01-be2d-7314e682fec3/solutions/55565413-c975-4f26-906d-acf84f744459/sol.py
+++ exercises/0605747c-c302-4f01-be2d-7314e682fec3/solutions/55565413-c975-4f26-906d-acf84f744459/sol.py
@@ -2,5 +2,5 @@ while True:
     try:
         print(sum(list(map(int, input()))))
     except ValueError:
-        print('Miałes wprowadzić cyfrę!')
+        print('Miałes wprowadzić liczbę!')
         break
