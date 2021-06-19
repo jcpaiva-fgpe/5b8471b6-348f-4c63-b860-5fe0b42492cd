a4d8e9ad87ce7a92d86f625a007fd050c5af52e1
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-19 17:13:33 +0100

    updated file exercises/255e4115-2f3d-43de-90aa-380e720e2274/solutions/9097ac4d-3073-4be6-bbaa-671486969948/sol.py

--- exercises/255e4115-2f3d-43de-90aa-380e720e2274/solutions/9097ac4d-3073-4be6-bbaa-671486969948/sol.py
+++ exercises/255e4115-2f3d-43de-90aa-380e720e2274/solutions/9097ac4d-3073-4be6-bbaa-671486969948/sol.py
@@ -5,23 +5,17 @@ while True:
         sum_psl = sum([int(pesel[i])*j for i, j in enumerate([i for i in [1,3,7,9]*3][:-2])])
         plec, last = map(int, pesel[9:])
         r, m, d = map(str, [pesel[i:i+2] for i in range(0,5,2)])
-        
-    
     except ValueError: break
     
     status = 10 - int(str(sum_psl)[-1])
     if status == last:
        zwroty = 'Pani' if plec % 2 == 0 else 'Pan', 'a' if plec % 2 == 0 else 'y'
-       
        r = f'19{r}' if int(m) < 20 else f'20{r}'
        m = int(m) if int(m) < 20 else int(m) - 20
        m = f'0{m}' if m < 10 else str(m) 
-       
        data = f'{d}.{m}.{r}'
        from datetime import datetime
        tday = datetime.now()
        wiek = (tday.year - int(r)) - ((tday.month, tday.day) < (int(m), int(d)))
-
     else: break
-
     print(zwroty[0], f'{name.title()}, urodzon{zwroty[1]}: {data}, legitymując{zwroty[1]} się numerem PESEL: {pesel}, aktualny wiek w latch: {wiek}')
