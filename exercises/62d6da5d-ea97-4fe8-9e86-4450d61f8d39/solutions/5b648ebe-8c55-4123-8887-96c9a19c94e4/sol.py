b671f9c96cf7b9188aade0f8317d4d11a27e2163
Thomas Naprawski <tnaprawski@gmail.com>
2021-06-19 12:51:54 +0100

    updated file exercises/62d6da5d-ea97-4fe8-9e86-4450d61f8d39/solutions/5b648ebe-8c55-4123-8887-96c9a19c94e4/sol.py

--- exercises/62d6da5d-ea97-4fe8-9e86-4450d61f8d39/solutions/5b648ebe-8c55-4123-8887-96c9a19c94e4/sol.py
+++ exercises/62d6da5d-ea97-4fe8-9e86-4450d61f8d39/solutions/5b648ebe-8c55-4123-8887-96c9a19c94e4/sol.py
@@ -1,1 +1,1 @@
-print('Twoje tymczasowe hasło to: ', sep="", *list(map(lambda x:int(x)*2, input("Poniżej wpisz datę w formacie DD.MM.RRRR:\n").split('.'))))
+print('Twoje tymczasowe hasło to: ', sep="", *list(map(lambda x: int(x)*2, input("Poniżej wpisz datę w formacie DD.MM.RRRR:\n").split('.'))))
