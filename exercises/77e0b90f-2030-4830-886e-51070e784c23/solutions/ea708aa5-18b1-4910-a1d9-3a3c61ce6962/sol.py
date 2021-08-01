6d761611f1072d6ad5b1b89cf38b33d095fd9bb9
Thomas Naprawski <tnaprawski@gmail.com>
2021-08-01 12:26:33 +0100

    Removed exercises/77e0b90f-2030-4830-886e-51070e784c23/solutions/ea708aa5-18b1-4910-a1d9-3a3c61ce6962/sol.py

--- exercises/77e0b90f-2030-4830-886e-51070e784c23/solutions/ea708aa5-18b1-4910-a1d9-3a3c61ce6962/sol.py
+++
@@ -1,8 +0,0 @@
-while True:
-    try: 
-        n = list(map(int, input('Wprowadź ciąg liczb całkowitych:​\n').split(',')))    
-    except ValueError: 
-        break
-
-    print('liczby to:', *sorted(set(n), reverse=True))
-    
